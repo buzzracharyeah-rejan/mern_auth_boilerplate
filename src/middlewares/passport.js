035984d5d2eeba04b5e7304c8853336675a63a5e
rejan buzzracharyeah <rejandev@gmail.com>
2022-03-08 23:04:07 +0545

    passport-jwt wip

--- src/middlewares/passport.js
+++ src/middlewares/passport.js
@@ -1,32 +1,49 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const userModel = require('../models/user');
+const { JWT_ACCESS_TOKEN_KEY } = require('../configs');
 
 const customFields = {
   usernameField: 'email',
   passwordField: 'password',
 };
 
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = JWT_ACCESS_TOKEN_KEY;
+
+passport.use(
+  'jwt',
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+        console.log('jwt test here'); 
+      const user = await userModel.findOne({ _id: jwt_payload.userId });
+      if (!user) return done(null, false);
+
+      return done(false, { userId: 'user1_test', username: 'testuser' });
+    } catch (error) {
+      return done(error, false);
+    }
+  })
+);
+
 passport.use(
   'local',
-  new LocalStrategy(
-    customFields,
-    async (email, password, cb) => {
-      try {
-          console.log('local st in use'); 
-          console.log(email, password); 
-        const user = await userModel.findOne({ email }).select('_id password');
-        if (!user) return cb(null, false, { message: 'Invalid email or password' });
+  new LocalStrategy(customFields, async (email, password, cb) => {
+    try {
+      const user = await userModel.findOne({ email }).select('_id password');
+      if (!user) return cb(null, false, { message: 'Invalid email or password' });
 
-        const isValid = await user.authenticate(password);
-        if (!isValid) return cb(null, false, { message: 'Invalid email or password' });
+      const isValid = await user.authenticate(password);
+      if (!isValid) return cb(null, false, { message: 'Invalid email or password' });
 
-        return cb(null, user)
-      } catch (error) {
-        cb(error);
-      }
+      return cb(null, user);
+    } catch (error) {
+      cb(error);
     }
-  )
+  })
 );
 
 module.exports = {
@@ -40,6 +57,11 @@ module.exports = {
       }
       req.user = user;
       next();
+    })(req, res, next);
+  },
+  passportJwt: async (req, res, next) => {
+    passport.authenticate('jwt', { session: false }, (err, user) => {
+        console.log(err, user)
     })(req, res, next);
   },
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController')
-const {validateBody, validateQuery} = require('../middlewares/validator')
-const {signupSchema,loginSchema,  tokenSchema} = require('../middlewares/schema/authSchema')
-const {passportLocal} = require('../middlewares/passport'); 
+const authController = require('../controllers/authController');
+const { validateBody, validateQuery } = require('../middlewares/validator');
+const { signupSchema, loginSchema, tokenSchema } = require('../middlewares/schema/authSchema');
+const { passportLocal, passportJwt } = require('../middlewares/passport');
 
 router.post('/signup', validateBody(signupSchema), authController.signup);
-router.post('/signupWithEmail', validateBody(signupSchema), authController.signupWithEmail); 
-router.post('/signupWithEmailConfirm', validateQuery(tokenSchema), authController.signupWithEmailConfirm)
-router.post('/login', validateBody(loginSchema), passportLocal, authController.login); 
+router.post('/signupWithEmail', validateBody(signupSchema), authController.signupWithEmail);
+router.post(
+  '/signupWithEmailConfirm',
+  validateQuery(tokenSchema),
+  authController.signupWithEmailConfirm
+);
+router.post('/login', validateBody(loginSchema), passportLocal, authController.login);
+router.get('/protected', passportJwt, (req, res, next) => {
+    // console.log(req.headers)
+  res.status(200).json({ status: 'success' });
+});
 
 module.exports = router;
